a3f7b3a5e84dc0f345e50be98e28705c265eb8d3
akmal <akmalqa101@gmail.com>
2019-04-20 23:14:18 -0700

    express ejs bodyparser

--- app.js
+++ app.js
@@ -1,46 +1,72 @@
 /*jshint esversion:6*/
 const express = require("express");
 const app =express();
+const bodyParser = require("body-parser");
+const ejs =require("ejs");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+const friends = ["Alex", "Jane", "Jake", "Felix"];
 
 app.get("/", function(req,res){
-  res.send("Hi there, welcome to my assignment!");
+  res.render("home");
 });
 
-app.get("/speak/:route",function(req,res){
+app.post("/addFriend",function(req,res){
+const newFriend = req.body.newFriend;
+friends.push(newFriend);
 
-  console.log(Object.values(req.params));
-  if(Object.values(req.params)==="pig"){
-    res.send("The pig says 'Oink'!");
-  }else if(Object.values(req.params)==="cow"){
-    res.send("The cow says 'Moo'");
-  }else if(Object.values(req.params)==="dog"){
-    res.send("The dog says 'Woof Woof'");
-  }
-});
-
-app.get("/repeat/:link/:num", function(req,res){
-
-  function repeatMe(str , num){
-    str = req.params.link;
-    num = req.params.num;
-  if (num<1){
-    str =" ";
-  }else{
-    return (str+" ").repeat(num);
-  }
-
-  }
-  // console.log(req.params.link);
-  // console.log(req.params.num);
-res.send(repeatMe(" link ", +" num "));
-
+  // res.send("Holldada");
+  res.redirect("friendsList");
 });
 
 
-app.get("*",function(req,res){
-  res.send("Sorry, page not found... What are you doing with your life?!");
+app.get("/friendsList", function(req,res){
+res.render("friends", {friends:friends}); //value on the left goes to ejs file value on the right goes to app.js
 });
+
+
+// app.get("/", function(req,res){
+//   res.send("Hi there, welcome to my assignment!");
+// });
+//
+// app.get("/speak/:route",function(req,res){
+//
+//   console.log(Object.values(req.params));
+//   if(Object.values(req.params)==="pig"){
+//     res.send("The pig says 'Oink'!");
+//   }else if(Object.values(req.params)==="cow"){
+//     res.send("The cow says 'Moo'");
+//   }else if(Object.values(req.params)==="dog"){
+//     res.send("The dog says 'Woof Woof'");
+//   }
+// });
+//
+// app.get("/repeat/:link/:num", function(req,res){
+//
+//   function repeatMe(str , num){
+//     str = req.params.link;
+//     num = req.params.num;
+//   if (num<1){
+//     str =" ";
+//   }else{
+//     return (str+" ").repeat(num);
+//   }
+//
+//   }
+//   // console.log(req.params.link);
+//   // console.log(req.params.num);
+// res.send(repeatMe(" link ", +" num "));
+//
+// });
+//
+//
+// app.get("*",function(req,res){
+//   res.send("Sorry, page not found... What are you doing with your life?!");
+// });
 
 
 app.listen(3000,function(){
